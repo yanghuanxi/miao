ee4c04c252e021b2a367bac34c24df4d2325dea9
yanghuanxi <bilei_lei@163.com>
2019-01-10 14:09:50 +0800

    add task lodash

---
+++ lodash/yanghuanxi-lodash.js
@@ -0,0 +1,19 @@
+// https://USERNAME.github.io/miao/lodash/USERNAME-lodash.js
+var tangibilizing = {
+  chunk: function chunk(ary, size = 1) {
+    var result = []
+    for(var i = 0; i< ??; i++) {
+      result.push(ary.slice(i, i + size))
+    }
+    return result
+  },
+  compact: function(ary) {
+    var result = []
+    for(var item of ary) {
+      if (item) {
+        result.push(item)
+      }
+    }
+    return result
+  }
+}
